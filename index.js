c992e35ac0ea2a93def29a4c8053d0e62844803d
Kyle Euman <euman_kyle@yahoo.com>
2021-04-03 13:16:00 -0400

    more changes to script and added test for engineer

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const lib =require('lib');
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const employees = [];
+
 
 
 
