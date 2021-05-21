188218424278e189d978b7e9f581ae8c7b14e181
Kyle Euman <euman_kyle@yahoo.com>
2021-05-21 18:56:09 -0400

    got app to finally start working

--- Main/__tests__/Engineer.test.js
+++ Main/__tests__/Engineer.test.js
@@ -8,12 +8,12 @@ test("Can set GitHUb account via constructor", () => {
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Jim", 1, "testjim@gmail.com", "GitHubUser");
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Jim", 1, "testjim@gmail.com", testValue);
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
 });
